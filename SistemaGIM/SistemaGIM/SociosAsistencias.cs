3b8ed4f6ec6dd87d5fca0f9267617ec640e4f3f8
bermudez.ferreira.jess <bermudez.ferreira.jess@hotmail.com>
2018-11-23 18:13:37 -0600

    socios asistencia

--- SistemaGIM/SistemaGIM/SociosAsistencias.cs
+++ SistemaGIM/SistemaGIM/SociosAsistencias.cs
@@ -21,6 +21,15 @@ namespace SistemaGIM
         private void SociosAsistencias_Load(object sender, EventArgs e)
         {
             pnlRegistros.Enabled = false;
+            socioAsistenciaTableAdapter.Fill(ds.SocioAsistencia,null,null,"","","",null,"","",null,null,null,null,1);
+            socioTableAdapter.Fill(ds.Socio,null,"","","",null,"","",null,1);
+
+
+        }
+        private void actualizar() {
+
+            socioAsistenciaTableAdapter.Fill(ds.SocioAsistencia, null, null, "", "", "", null, "", "", null, null, null, null, 1);
+            socioTableAdapter.Fill(ds.Socio, null, "", "", "", null, "", "", null, 1);
         }
 
         private void btnInsetar_Click(object sender, EventArgs e)
@@ -40,18 +49,37 @@ namespace SistemaGIM
         private void btnEliminar_Click(object sender, EventArgs e)
         {
             //eliminar se agregar despues de conectar la base de datos
+
+            DialogResult resultado;
+            int seleccionado_SocioAsistencia = dataSocioAsistencia.CurrentRow.Index;
+            resultado = MessageBox.Show("Se Eliminara el registro ", "¿Esta seguro?", MessageBoxButtons.YesNo);
+
+            if (resultado == System.Windows.Forms.DialogResult.Yes)
+            {
+                socioAsistenciaTableAdapter.Delete((int)dataSocioAsistencia.Rows[seleccionado_SocioAsistencia].Cells[0].Value);
+
+                //this.Close();
+
+            }
+            this.actualizar();
         }
+
 
         private void button2_Click(object sender, EventArgs e)
         {
+            int seleccionado = dataSocioAsistencia.CurrentRow.Index;
+            int seleccionado2 = dataSocio.CurrentRow.Index;
+            
             if (estado == "insertar")
             {
-
+                
+                
+              //  socioAsistenciaTableAdapter.Insert((int)dataSocio.Rows[seleccionado].Cells[0].Value,dt_fecha.Value,he_dateTimePicker,hs_dateTimePicker,1);
 
             }
             if (estado == "editar")
             {
-
+                //socioAsistenciaTableAdapter.Update((int)dataSocioAsistencia.Rows[seleccionado2].Cells[0].Value, (int)dataSocio.Rows[seleccionado].Cells[0].Value ,dt_fecha.Value, he_dateTimePicker, hs_dateTimePicker, 1);
 
             }
             pnlBotones.Enabled = true;
@@ -63,6 +91,11 @@ namespace SistemaGIM
         {
             pnlBotones.Enabled = true;
             pnlRegistros.Enabled = false;
+        }
+
+        private void pnlRegistros_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
